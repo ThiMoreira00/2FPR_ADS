95a07d1898798359145501496fc47094f8ec13d0
Thiago M <129688942+ThiMoreira00@users.noreply.github.com>
2023-05-18 22:32:06 -0300

    Create ListaV_Ex001.c

---
+++ ListaV_Matrizes/Ex001/ListaV_Ex001.c
@@ -0,0 +1,36 @@
+/*
+
+	FAETERJ-Rio
+	2FPR - Fundamentos de Programação para Computação
+	Prof.° Leonardo Vianna
+	
+	Lista de Exercícios V - Matrizes
+	
+	
+	QUESTÃO 01:
+	Faça uma função que, dada uma matriz M8×5 de reais, gere a matriz M^t, sua transposta.
+	
+*/
+
+// Importação de bibliotecas
+#include <stdio.h>
+
+// Definição de constantes
+#define L 8
+#define C 5
+
+// Protótipo das funções
+int gerarMatriz(float matrizInicial[L][C], float matrizFinal[L][C]) {
+	
+}
+
+// Função main
+void main() {
+	
+	
+	float matriz1[L][C];
+
+	// chamando a função
+	gerarMatriz
+}
+
